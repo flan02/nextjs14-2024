caee8498a94e7f8d1702b4f5585b5a9f62ee786c
flan02 <chanivetdan@hotmail.com>
2024-04-04 13:51:47 -0300

    1hs13min reached. Lets take a rest.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,54 @@
-import Image from "next/image"
+import Image from "next/image";
+import styles from "./about.module.css";
+
+export const metadata = {
+  title: "About Page",
+  description: "About description",
+};
 
 
+const AboutPage = () => {
 
-type Props = {}
-
-const Aboutpage = (props: Props) => {
+  // console.log("lets check where it works")
   return (
-    <div>
-      <Image src="/about.png" alt="about-image" width={300} height={300} />
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/about.png"
+          alt="About Image"
+          fill
+          sizes="(max-width: 600px) 100vw, 600px"
+          className={styles.img}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Aboutpage
+export default AboutPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
-
+import Image from "next/image";
+import styles from "./home.module.css";
 
 export default function Home() {
 
   return (
-    <main>
-      <h1>Homepage</h1>
+    <main className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Finance</h1>
+        <p className={styles.desc}>
+          Control your finances with ease.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+
+        <div className={styles.brands}>
+          <Image
+            sizes="(max-width: 600px) 100vw, 600px"
+            src="/brands.png" alt="brands" fill className={styles.brandImg} />
+        </div>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <Image
+          src="/hero.gif"
+          alt="Homepage Image"
+
+          sizes="(max-width: 600px) 100vw, 400px"
+          width={500}
+          height={500}
+          className={styles.heroImg}
+        />
+      </div>
+
     </main>
 
   );
